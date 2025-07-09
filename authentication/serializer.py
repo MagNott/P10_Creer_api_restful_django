1b7189c5f7b6992b6d4da81438b6b59562095a1b
MagNott <notteghem.magali@gmail.com>
2025-07-09 19:06:49 +0200

    #3 Age minimum requis inscription user - tests postman OK

--- authentication/serializer.py
+++ authentication/serializer.py
@@ -1,5 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from .models import CustomUser
+from datetime import date
+from rest_framework.serializers import ValidationError
 
 
 class CustomUserSerializer(ModelSerializer):
@@ -14,3 +16,20 @@ class CustomUserSerializer(ModelSerializer):
             'can_be_contacted',
             'can_data_be_shared'
         ]
+
+    def validate_date_birth(self, input_date_birth):
+
+        today = date.today()
+
+        # Calcul de l'âge en années
+        age = today.year - input_date_birth.year
+
+       # Vérifie si l'anniversaire est encore à venir cette année
+        if (today.month, today.day) < (input_date_birth.month, input_date_birth.day):
+            age -= 1  # on enlève 1 à l'âge
+
+        # On vérifie l'âge minimum
+        if age < 15:
+            raise ValidationError("Vous devez avoir au moins 15 ans pour vous inscrire.")
+
+        return input_date_birth
--- authentication/views.py
+++ authentication/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import CustomUser
 from .serializer import CustomUserSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class CustomUserViewSet(ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+
+    # A remettre lorsque j'aurai géré les JWT, je m'auto bloque pour les tests avec postman sinon
+    # permission_classes = [IsAuthenticated]
