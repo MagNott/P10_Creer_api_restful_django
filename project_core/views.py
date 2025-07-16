04cc6766f9e518564f06dfe22222b3b5f9ba63cc
MagNott <notteghem.magali@gmail.com>
2025-07-16 21:46:50 +0200

    #9 get un projet + delete  + patch +put OK pour projets

--- project_core/views.py
+++ project_core/views.py
@@ -5,9 +5,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
+# Pour la route api/projects/
 class ProjectView(APIView):
-    model_project = Project
-    serializer = ProjectSerializer
 
     def post(self, request):
         serializer_project = ProjectSerializer(data=request.data)
@@ -30,3 +29,43 @@ class ProjectView(APIView):
 
         return Response(serializer_project.data, status=200)
 
+
+# Pour la route api/projects/id
+class ProjectDetailView(APIView):
+
+    def get(self, request, project_id):
+        selected_project = Project.objects.get(pk=project_id)
+
+        if not Contributor.objects.filter(user=request.user, project=selected_project).exists():
+            return Response({"detail": "Accès interdit"}, status=403)
+
+        serializer = ProjectSerializer(selected_project)
+        return Response(serializer.data)
+
+    def delete(self, request, project_id):
+        selected_project = Project.objects.get(pk=project_id)
+        if selected_project.author == request.user:
+            selected_project.delete()
+            return Response(status=204)
+        else:
+            return Response({"detail": "Accès interdit"}, status=403)
+
+    def patch(self, request, project_id):
+        selected_project = Project.objects.get(pk=project_id)
+        if selected_project.author == request.user:
+            project_serializer = ProjectSerializer(selected_project, data=request.data, partial=True)
+            if project_serializer.is_valid():
+                project_serializer.save()
+                return Response(project_serializer.data, status=200)
+            else:
+                return Response(project_serializer.errors, status=400)
+
+    def put(self, request, project_id):
+        selected_project = Project.objects.get(pk=project_id)
+        if selected_project.author == request.user:
+            project_serializer = ProjectSerializer(selected_project, data=request.data)
+            if project_serializer.is_valid():
+                project_serializer.save()
+                return Response(project_serializer.data, status=200)
+            else:
+                return Response(project_serializer.errors, status=400)
