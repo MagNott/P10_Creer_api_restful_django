2e852756fd1586bbd5cd56430914e77af5bae752
MagNott <notteghem.magali@gmail.com>
2025-07-16 16:55:29 +0200

    #9 Modif post projets pour ajout contributeur + get tous les projets du contributeur

--- project_core/views.py
+++ project_core/views.py
@@ -6,17 +6,27 @@ from rest_framework.response import Response
 
 
 class ProjectView(APIView):
-    model = Project
+    model_project = Project
     serializer = ProjectSerializer
 
     def post(self, request):
-        # 1. Sérialiser et valider les données entrantes
-        serializer = ProjectSerializer(data=request.data)
+        serializer_project = ProjectSerializer(data=request.data)
 
-        # 2. Si les données sont valides
-        if serializer.is_valid():
-            serializer.save(author=request.user)  # ← ici, on crée le projet
-            return Response(serializer.data, status=201)
+        if serializer_project.is_valid():
+            new_project = serializer_project.save(author=request.user)
+            Contributor.objects.create(user=request.user, project=new_project)
+            return Response(serializer_project.data, status=201)
 
-        # Sinon, on renvoie une erreur
-        return Response(serializer.errors, status=400)
+        return Response(serializer_project.errors, status=400)
+
+    def get(self, request):
+        projects_contributor = Contributor.objects.filter(user=request.user)
+
+        projects = []
+        for project_contributor in projects_contributor:
+            projects.append(project_contributor.project)
+
+        serializer_project = ProjectSerializer(projects, many=True)
+
+        return Response(serializer_project.data, status=200)
+
