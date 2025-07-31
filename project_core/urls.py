a41ed8dda530fcfd94500f441441cef27b16460d
MagNott <notteghem.magali@gmail.com>
2025-07-31 20:36:11 +0200

    #17 routes des comments gérées et testées OK

--- project_core/urls.py
+++ project_core/urls.py
@@ -5,6 +5,8 @@ from .views.contributor_views import ContributorView
 from .views.contributor_views import ContributorDetailView
 from .views.issue_views import IssueView
 from .views.issue_views import IssueDetailView
+from .views.comment_views import CommentView
+from .views.comment_views import CommentDetailView
 
 urlpatterns = [
     path("", ProjectView.as_view(), name="project-list-create"),
@@ -30,5 +32,15 @@ urlpatterns = [
         "<int:project_id>/issues/<int:issue_id>/",
         IssueDetailView.as_view(),
         name="issue-detail"
+    ),
+    path(
+        "<int:project_id>/issues/<int:issue_id>/comments/",
+        CommentView.as_view(),
+        name="comment-list-create",
+    ),
+    path(
+        "<int:project_id>/issues/<int:issue_id>/comments/<uuid:comment_id>/",
+        CommentDetailView.as_view(),
+        name="comment-detail"
     ),
 ]
